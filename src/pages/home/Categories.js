ec79fed010c54ab015bb807bd99b3c677e3eb61b
Arshad Danish <arshaddanishthana@gmail.com>
2022-08-01 00:10:24 +0530

    update

--- src/pages/home/Categories.js
+++ src/pages/home/Categories.js
@@ -4,6 +4,32 @@ export default function Categories() {
   return (
     <div className="categories">
       <h2>Categories</h2>
+      <div className="items">
+        <div className="car">
+          <div></div>
+          <h3>Cars</h3>
+        </div>
+        <div className="house">
+          <div></div>
+          <h3>Apartments</h3>
+        </div>
+        <div className="machine">
+          <div></div>
+          <h3>Machines</h3>
+        </div>
+        <div className="house">
+          <div></div>
+          <h3>Boats</h3>
+        </div>
+        <div className="machine">
+          <div></div>
+          <h3>Cars</h3>
+        </div>
+        <div className="car">
+          <div></div>
+          <h3>Others</h3>
+        </div>
+      </div>
     </div>
   );
 }
