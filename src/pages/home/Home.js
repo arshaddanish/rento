29d2e6292c4d2ff00d16b46ecd8db3ec5f45d93a
Arshad Danish <arshaddanishthana@gmail.com>
2022-08-01 00:15:24 +0530

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Categories from "./Categories";
 import Landing from "./Landing";
 import "./home.scss";
+import News from "./News";
 
 export default function Home() {
   return (
     <>
       <Landing />
       <Categories />
+      <News />
     </>
   );
 }
---
+++ src/pages/home/News.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function News() {
+  return (
+    <div className="news">
+      <h2>News & Articles</h2>
+      <p>Here comes news & articles from the RENTO team.</p>
+      <div></div>
+    </div>
+  );
+}
