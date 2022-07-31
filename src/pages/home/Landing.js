0364ad753136d491cfce06fac71ecb9cf14f3192
Arshad Danish <arshaddanishthana@gmail.com>
2022-07-31 22:32:37 +0530

    update

--- src/pages/home/Landing.js
+++ src/pages/home/Landing.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./home.scss";
+import TextField from "@mui/material/TextField";
 
 export default function Landing() {
   return (
     <div className="landing">
-      <div className="container">
-        <h1>All In One Rental Market Place !</h1>
-        <p>We have what you need from tools to apartments.</p>
+      <h1>All In One Rental Market Place !</h1>
+      <p>We have what you need from tools to apartments.</p>
+      <div className="search-box">
+        <div>
+          <TextField
+            label="Product/ Category"
+            variant="outlined"
+            style={{ width: 250 }}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Location"
+            variant="outlined"
+            style={{ width: 300 }}
+          />
+        </div>
+        <button className="find-btn">Find</button>
       </div>
     </div>
   );
