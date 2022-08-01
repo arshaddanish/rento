1bd9f9085dfda8c45f74c2aa759f71019c8fd5ca
Arshad Danish <arshaddanishthana@gmail.com>
2022-08-01 21:41:26 +0530

    update

--- src/pages/home/News.js
+++ src/pages/home/News.js
@@ -1,11 +1,35 @@
 import React from "react";
+import mer4 from "../../assets/images/news/mer4.jpg";
+
+function Item() {
+  return (
+    <div className="item">
+      <div className="img-div">
+        <img src={mer4} alt="" />
+        <div className="date">
+          <p>19 Oct</p>
+        </div>
+      </div>
+      <div className="dtl">
+        <p>Car Lifestyle</p>
+        <h3>How to use Map in Audi A6 luxury?</h3>
+      </div>
+    </div>
+  );
+}
 
 export default function News() {
   return (
     <div className="news">
-      <h2>News & Articles</h2>
-      <p>Here comes news & articles from the RENTO team.</p>
-      <div></div>
+      <div className="title-div">
+        <h2>News & Articles</h2>
+        <button>View All</button>
+      </div>
+      <div className="items">
+        {Item()}
+        {Item()}
+        {Item()}
+      </div>
     </div>
   );
 }
