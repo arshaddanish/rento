2695e90f8dd62f540ccac27d9118e6ef674eb770
Sidharth <sidharth.a.satheesh@gmail.com>
2022-09-11 13:09:53 +0530

    Login & Register MUI

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,5 @@
-// import React from 'react'
-// import "./login.scss"
-
-// export default function Login() {
-//   return (
-//     <div className='login'>Login</div>
-//   )
-// }
-// import TextField from "@mui/material/TextField";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 import image from '../../images/login1.png'
 import logo from '../../images/logo.png'
 import React from 'react'
@@ -34,10 +27,24 @@ const Login = () => {
                 <h1>Log In</h1>
                 <p>or <Link to="/signup"><span className='link'>Create an account</span></Link></p>
                 <form action="">
-                    <input placeholder='Phone number' type="number" name="" id="" />
-                    <input placeholder='Password' type="password" name="" id="" />
+                
+                    <TextField fullWidth
+                    // id="outlined-basic1" 
+                    label="Phone Number" 
+                    type={"number"}
+                    variant="outlined" />
+                    <div className="gap"></div>
+                <TextField 
+                    fullWidth 
+                    // id="outlined-basic" 
+                    label="Password" 
+                    type={"password"}
+                    variant="outlined" />
+                    {/* <input placeholder='Phone number' type="number" name="" id="" />
+                    <input placeholder='Password' type="password" name="" id="" /> */}
                     <Link to="/forgot" className='forgot-pass-btn'>Forgot password?</Link>
-                    <button onClick={navigateToAccount} type='submit'>Login</button>
+                    <Button onClick={navigateToAccount} variant="contained">Login</Button>
+                    {/* <button onClick={navigateToAccount} type='submit'>Login</button> */}
                 </form>
                 
             </div>
--- src/pages/login/SignUp.js
+++ src/pages/login/SignUp.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Button, TextField } from "@mui/material";
 
 import { Link,useNavigate } from "react-router-dom";
 import './SignUp.scss'
 import handshake from '../../images/handshake.jpg'
+
 
 const SignUp = () => {
   const navigate = useNavigate();
   const navigateToHome = () => {
     navigate('/');
   }
-  
+
   return (
     <div>
       <div className='main-signup'>
@@ -21,16 +23,94 @@ const SignUp = () => {
                 <h1>Lets get started</h1>
                 <p>or <Link to="/login"><span className='link'>Login to your account</span></Link></p>
                 <form action="">
-                <input placeholder='Name' type="text" name="" id="" />
-                    <input placeholder='Email' type="email" name="" id="" />
+                {/* <input placeholder='Name' type="text" name="" id="" /> */}
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Name" 
+                  type={"text"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Email" 
+                  type={"email"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Phone Number" 
+                  type={"number"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="House Name" 
+                  type={"text"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Locality" 
+                  type={"text"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="City" 
+                  type={"text"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Pincode" 
+                  type={"number"}
+                  variant="outlined" 
+                />
+                <div className="gap"></div>
+                {/* <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Date of Birth" 
+                  type={"date"}
+                  variant="outlined" 
+                /> */}
+                <TextField
+                  name="someDate"
+                  label="Date of Birth"
+                  InputLabelProps={{ shrink: true, required: true }}
+                  type="date"
+                />
+                <div className="gap"></div>
+                <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Password" 
+                  type={"password"}
+                  variant="outlined" 
+                />
+                    <div className="gap"></div>
+                {/* <TextField fullWidth
+                  // id="outlined-basic1" 
+                  label="Confirm Password" 
+                  type={"password"}
+                  variant="outlined" 
+                /> */}
+                
+                    {/* <input placeholder='Email' type="email" name="" id="" />
                     <input placeholder='Phone number' type="number" name="" id="" />
                     <input placeholder='House Name' type="text" name="" id="" />
                     <input placeholder='Locality' type="text" name="" id="" />
                     <input placeholder='City' type="text" name="" id="" />
                     <input placeholder='Pincode' type="number" name="" id="" />
                     <input placeholder='Date of Birth' type="date" name="" id="" />
-                    <input placeholder='Password' type="password" name="" id="" />
-                    <button onClick={navigateToHome} type='submit'>Create Account</button>
+                    <input placeholder='Password' type="password" name="" id="" /> */}
+                    {/* <button onClick={navigateToHome} type='submit'>Create Account</button> */}
+                    
+                    <Button onClick={navigateToHome} variant="contained">Create Account</Button>
                 </form>
             </div>
         </div>
