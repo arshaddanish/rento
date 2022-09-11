4a7dcb5b4abc8c34c442b5505d698c97274bb8b1
Sidharth <sidharth.a.satheesh@gmail.com>
2022-09-11 18:03:03 +0530

    Forgot pass

--- src/pages/login/ForgotPass.js
+++ src/pages/login/ForgotPass.js
@@ -1,22 +1,60 @@
 import React from 'react'
-import logo from '../../images/logo.png'
+// import logo from '../../images/logo.png'
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import './forgotPass.scss'
+import { useNavigate } from 'react-router-dom';
 
 const ForgotPass = () => {
+    const navigate = useNavigate();
+    const navigateToSignUp=()=>{
+        navigate('/signup')
+    }
+    const navigateToLogin=()=>{
+        navigate('/login')
+    }
     return (
         <div className='forgot-pass'>
-            <div className='logo'>
-                <img src={logo} alt="" />
-            </div>
-            <div className='forgot-input'>
-                <form action="">
-                    <TextField style={{width: 400}} id="outlined-basic" label="Email" variant="outlined"  fullWidth required type="email"/>
-                    <div className='forgot-submit-btn'>
-                        <Button style={{width: 400}} type='submit' variant="contained">SUBMIT</Button>
+            <div className="forgot-pass-main">
+                <div className="forgot-pass-sub">
+                    <div className='logo'>
+                        {/* <img src={logo} alt="" /> */}
+                        <div className="forgot-lock-img">
+                
+                            <i className="fa-solid fa-lock"></i>
+                        </div>
+                
                     </div>
-                </form>
+                    <div className='forgot-input'>
+                
+                    <h3 className='forgot-heading'>Trouble Logging In?</h3>
+                    <p className='forgot-desc'>Enter your email and we'll send you a link to reset your password.</p>
+                    {/* <p className='back-to-login'>Back to Login</p> */}
+                        <form action="">
+                            <TextField autoComplete='off' fullWidth id="outlined-basic" label="Email" variant="outlined" required type="email"/>
+                            <div className='forgot-submit-btn'>
+                                <Button fullWidth type='submit' variant="contained">GET RESET LINK</Button>
+                            </div>
+                        </form>
+                        <p className='forgot-or'>OR</p>
+                        <p onClick={navigateToSignUp} className='forgot-create'>Create New Account</p>
+                    </div>
+                </div>
+                <Button 
+                className='forgot-back-to-login-btn'
+                    sx={{
+                        backgroundColor: "#eeeeee", 
+                        color: "#707070",
+                        borderTop: "1px solid #d2d0d0",
+                        '&:hover': {
+                            backgroundColor: '#eeeeee',
+                            color: '#000',
+                        },
+                    }} 
+                    onClick={navigateToLogin}
+                    fullWidth 
+                    variant="contained">
+                    Back to login</Button>
             </div>
         </div>
     ) 
