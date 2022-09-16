dd60e9c67dd08df399de7714650200136393a105
Arshad Danish <arshaddanishthana@gmail.com>
2022-09-17 01:28:36 +0530

    up

--- src/admin/pages/public-messages/PublicMessages.js
+++ src/admin/pages/public-messages/PublicMessages.js
@@ -1,8 +1,66 @@
-import React from 'react'
-import "./publicMessages.scss"
+import React from "react";
+import "./publicMessages.scss";
+import Paper from "@mui/material/Paper";
 
 export default function PublicMessages() {
   return (
-    <div className='public-messages'>Public Messages</div>
-  )
+    <div className="public-messages">
+      <h2>Messages</h2>
+      <div className="messages-table">
+        <Paper elevation={1} className="paper">
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            voluptates incidunt, explicabo beatae aut nobis enim laboriosam
+            accusantium, quae repellat praesentium, fugit officiis numquam culpa
+            sunt facilis atque illo? Voluptas dolorum nam laudantium cumque
+            pariatur architecto enim soluta repudiandae libero, quidem
+            distinctio placeat, sed explicabo commodi obcaecati temporibus natus
+            doloremque maiores eius sequi facilis. Possimus commodi pariatur
+            reiciendis blanditiis, laborum reprehenderit numquam dignissimos
+            dicta soluta illum nihil enim asperiores expedita quibusdam. Error
+            ea est iusto enim, eum laudantium, quis minima quisquam, minus nobis
+            tenetur perspiciatis rerum. Aliquid praesentium porro vero repellat,
+            dolorum dolorem delectus distinctio? Nostrum dolore inventore eius
+            sed.
+          </div>
+        </Paper>
+
+        <Paper elevation={1} className="paper">
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            voluptates incidunt, explicabo beatae aut nobis enim laboriosam
+            accusantium, quae repellat praesentium, fugit officiis numquam culpa
+            sunt facilis atque illo? Voluptas dolorum nam laudantium cumque
+            pariatur architecto enim soluta repudiandae libero, quidem
+            distinctio placeat, sed explicabo commodi obcaecati temporibus natus
+            doloremque maiores eius sequi facilis. Possimus commodi pariatur
+            reiciendis blanditiis, laborum reprehenderit numquam dignissimos
+            dicta soluta illum nihil enim asperiores expedita quibusdam. Error
+            ea est iusto enim, eum laudantium, quis minima quisquam, minus nobis
+            tenetur perspiciatis rerum. Aliquid praesentium porro vero repellat,
+            dolorum dolorem delectus distinctio? Nostrum dolore inventore eius
+            sed.
+          </div>
+        </Paper>
+
+        <Paper elevation={1} className="paper">
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            voluptates incidunt, explicabo beatae aut nobis enim laboriosam
+            accusantium, quae repellat praesentium, fugit officiis numquam culpa
+            sunt facilis atque illo? Voluptas dolorum nam laudantium cumque
+            pariatur architecto enim soluta repudiandae libero, quidem
+            distinctio placeat, sed explicabo commodi obcaecati temporibus natus
+            doloremque maiores eius sequi facilis. Possimus commodi pariatur
+            reiciendis blanditiis, laborum reprehenderit numquam dignissimos
+            dicta soluta illum nihil enim asperiores expedita quibusdam. Error
+            ea est iusto enim, eum laudantium, quis minima quisquam, minus nobis
+            tenetur perspiciatis rerum. Aliquid praesentium porro vero repellat,
+            dolorum dolorem delectus distinctio? Nostrum dolore inventore eius
+            sed.
+          </div>
+        </Paper>
+      </div>
+    </div>
+  );
 }
