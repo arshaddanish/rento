69cc6d0afec3ca9449901c3a0156dac6d93c232e
Sidharth <sidharth.a.satheesh@gmail.com>
2022-09-18 13:01:43 +0530

    up

--- src/pages/account/subscriptions/Subscriptions.js
+++ src/pages/account/subscriptions/Subscriptions.js
@@ -1,6 +1,6 @@
 import './subscriptions.scss'
 import React from "react";
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 
 const Subscriptions = () => {
   const registrations = [
