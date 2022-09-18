a9bd4484a6d0cc426d8d4ddf1c329faa06a221f5
Arshad Danish <arshaddanishthana@gmail.com>
2022-09-19 02:15:22 +0530

    up

--- src/pages/account/seller-bookings/SellerBookings.js
+++ src/pages/account/seller-bookings/SellerBookings.js
@@ -1,43 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./sellerBookings.scss";
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import apis from "../../../apis";
+import { httpHeaders } from "../../../services/httpHeaders";
 
 const SellerBookings = () => {
-  const registrations = [
-    {
-      id: 1,
-      name: "Albin Vargees",
-      dept: "Computer Science Engineering",
-      batch: "2011",
-    },
-    {
-      id: 2,
-      name: "Sidharth A",
-      dept: "Electronics & Communication Engineering",
-      batch: "2001",
-    },
-    {
-      id: 3,
-      name: "Arshad Danish",
-      dept: "Mechanical Engineering",
-      batch: "2003",
-    },
-    {
-      id: 4,
-      name: "Jyothiradithyan K",
-      dept: "Civil Engineering",
-      batch: "2007",
-    },
-    {
-      id: 5,
-      name: "Rishan KP",
-      dept: "Computer Science Engineering",
-      batch: "2010",
-    },
-  ];
+  let navigate = useNavigate();
+
+  let [bookings, setBookings] = useState();
+  useEffect(() => {
+    fetchBookings();
+  }, []);
+  let fetchBookings = async () => {
+    let { data } = await apis.get("booking/seller", httpHeaders("user"));
+    // console.log(data);
+    setBookings(data);
+  };
+
+  let onItemClick = (item) => {
+    navigate("/categories/" + item.category.toLowerCase() + "/" + item._id);
+  };
+
   const [viewForm, setViewForm] = useState(false);
+
+  if (!bookings) return null;
   return (
-    <div className="seller-requests1">
+    <div className="seller-bookings1">
       <div className="registration-main2">
         <div>
           <h2 className="reg-heading-main">Bookings</h2>
@@ -51,17 +40,27 @@ const SellerBookings = () => {
             <div>Drop-off Date</div>
             <div>Quantity</div>
           </div>
-          {registrations.map((e) => (
+          {bookings.bookings.map((e, i) => (
             <div key={e.id} className="reg-data">
-              <div>{e.name}</div>
-              <div>{e.dept}</div>
-              <div>{e.batch}</div>
-              <div>{e.name}</div>
-              <div>{e.dept}</div>
-              <div>{e.dept}</div>
+              <div
+                onClick={() => onItemClick(bookings.item_info[i])}
+                className="item-link"
+              >
+                {bookings.item_info[i].name}
+              </div>
+              <div className="item-link">{bookings.buyer_info[i].name}</div>
+              <div>{e.bookingDate.substring(0, 10)}</div>
+              <div>{e.pickupDate.substring(0, 10)}</div>
+              <div>{e.dropoffDate.substring(0, 10)}</div>
+              <div>{e.quantity}</div>
               <div className="reg-btns">
-                <Button size="small" onClick={() => setViewForm(true)} variant="contained">
-                  View History
+                <Button
+                  size="small"
+                  color="success"
+                  onClick={() => setViewForm(true)}
+                  variant="contained"
+                >
+                  Payment History
                 </Button>
               </div>
             </div>
@@ -73,4 +72,3 @@ const SellerBookings = () => {
 };
 
 export default SellerBookings;
-
