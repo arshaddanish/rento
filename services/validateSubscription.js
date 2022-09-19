58ae0ee9f0972456555b4d0aaafadc0805ec9612
Arshad Danish <arshaddanishthana@gmail.com>
2022-09-19 14:02:22 +0530

    up

--- services/validateSubscription.js
+++ services/validateSubscription.js
@@ -1,18 +1,20 @@
-const {Subscription} = require("../models/subscription");
-const User = require("../models/user")
-const Item = require("../models/item")
+const { Subscription } = require("../models/subscription");
+const User = require("../models/user");
+const Item = require("../models/item");
 
 let validateSubscription = async () => {
-    const sellers = await User.find({ subStatus : 1})
-    // console.log(sellers);
+  const sellers = await User.find({ subStatus: 1 });
+  // console.log(sellers);
 
-    today = new Date()
-    for(let i = 0; i < sellers.length; i++){
-        const subscriptions = await Subscription.find({ sellerId: sellers[i]._id }).sort({ subDate : -1});
-        if(subscriptions[0].endDate < today){
-            await Item.updateMany({ sellerId : sellers[i]._id},{ status: 0 })
-        }
+  today = new Date();
+  for (let i = 0; i < sellers.length; i++) {
+    const subscriptions = await Subscription.find({
+      sellerId: sellers[i]._id,
+    }).sort({ subDate: -1 });
+    if (subscriptions[0] && subscriptions[0].endDate < today) {
+      await Item.updateMany({ sellerId: sellers[i]._id }, { status: 0 });
     }
+  }
 };
 
 module.exports = validateSubscription;
