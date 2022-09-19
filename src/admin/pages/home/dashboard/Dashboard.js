47ffc810c0608ae760a3b9b8d083674a825dbae7
Sidharth <sidharth.a.satheesh@gmail.com>
2022-09-19 11:02:36 +0530

    admin dash tile

--- src/admin/pages/home/dashboard/Dashboard.js
+++ src/admin/pages/home/dashboard/Dashboard.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./dashboard.scss";
+import { Paper } from "@mui/material";
 
 export default function Dashboard() {
-  return <div className="dashboard">Dashboard</div>;
+  return <div className="dashboard">
+    <Paper className="admin-dash-tile" elevation={3}>
+      <div className="admin-dash-tile-left">Active Buyers:</div><div className="admin-dash-tile-right">20</div>
+    </Paper>
+    <Paper className="admin-dash-tile" elevation={3}>
+      
+    <div className="admin-dash-tile-left">Active Sellers:</div><div className="admin-dash-tile-right">20</div>
+    </Paper>
+    <Paper className="admin-dash-tile" elevation={3}>
+      
+    <div className="admin-dash-tile-left">Active Buyers:</div><div className="admin-dash-tile-right">20</div>
+    </Paper>
+    <Paper className="admin-dash-tile" elevation={3}>
+      
+    <div className="admin-dash-tile-left">Active Buyers:</div><div className="admin-dash-tile-right">20</div>
+    </Paper>
+  </div>;
 }
