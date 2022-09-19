ef6297e47a29c36bb028ba3fff6aecd98402b03b
Rishan KP <rishangiggs11@gmail.com>
2022-09-19 12:29:50 +0530

    dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,16 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 const User = require("../models/user");
 const Item = require("../models/item");
-const {Subscription} = require("../models/subscription");
+const validateSubscription = require("../services/validateSubscription");
+const {Subscription,SubscriptionPlan} = require("../models/subscription");
 const { adminAuth } = require("../middleware/adminAuth");
 
 app.get("/api/dashboard", adminAuth, async (req, res) => {
+    validateSubscription();
     const userCount = await User.countDocuments();
     const buyerCount = await User.countDocuments({ verStatus:  "Not Verified" });
     const sellerCount = await User.countDocuments({ verStatus:  "Verified" });
     const itemCount = await Item.countDocuments({ status:  1 });
 
-    today = Date.now();
+    var today = Date.now();
+    var total = 0;
 
     const activeSellerCount = await Subscription.countDocuments({ endDate : { $gt: today } });
 
@@ -21,7 +24,20 @@ app.get("/api/dashboard", adminAuth, async (req, res) => {
         { $group: { _id: "$planId", count: { $sum: 1 } } },
     ])
 
-    console.log(subscriptions);
+    for (let i = 0; i < subscriptions.length; i++) {
+        const subscription = subscriptions[i];
+        const plan = await SubscriptionPlan.findById(subscription._id);
+        total = total + subscription.count * plan.amount;
+    }
+
+    res.status(200).send({
+        userCount: userCount,
+        buyerCount: buyerCount,
+        sellerCount: sellerCount,
+        itemCount: itemCount,
+        activeSellerCount: activeSellerCount,
+        subscriptionRevenue: total,
+    });
 });
 
 module.exports = app;
