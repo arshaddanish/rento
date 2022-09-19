0fe8650872efe8e705024a197c84b5a6896bc2ee
Arshad Danish <arshaddanishthana@gmail.com>
2022-09-19 12:51:23 +0530

    up

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,40 +4,42 @@ const mongoose = require("mongoose");
 const User = require("../models/user");
 const Item = require("../models/item");
 const validateSubscription = require("../services/validateSubscription");
-const {Subscription,SubscriptionPlan} = require("../models/subscription");
+const { Subscription, SubscriptionPlan } = require("../models/subscription");
 const { adminAuth } = require("../middleware/adminAuth");
 
 app.get("/api/dashboard", adminAuth, async (req, res) => {
-    validateSubscription();
-    const userCount = await User.countDocuments();
-    const buyerCount = await User.countDocuments({ verStatus:  "Not Verified" });
-    const sellerCount = await User.countDocuments({ verStatus:  "Verified" });
-    const itemCount = await Item.countDocuments({ status:  1 });
+  validateSubscription();
+  const userCount = await User.countDocuments();
+  const buyerCount = await User.countDocuments({ verStatus: "Not Verified" });
+  const sellerCount = await User.countDocuments({ verStatus: "Verified" });
+  const itemCount = await Item.countDocuments({ status: 1 });
 
-    var today = Date.now();
-    var total = 0;
+  var today = Date.now();
+  var total = 0;
 
-    const activeSellerCount = await Subscription.countDocuments({ endDate : { $gt: today } });
+  const activeSellerCount = await Subscription.countDocuments({
+    endDate: { $gt: today },
+  });
 
-    const subscriptions = await Subscription.aggregate([
-        { $match: { _id : { $exists: true } } },
-        { $group: { _id: "$planId", count: { $sum: 1 } } },
-    ])
+  const subscriptions = await Subscription.aggregate([
+    { $match: { _id: { $exists: true } } },
+    { $group: { _id: "$planId", count: { $sum: 1 } } },
+  ]);
 
-    for (let i = 0; i < subscriptions.length; i++) {
-        const subscription = subscriptions[i];
-        const plan = await SubscriptionPlan.findById(subscription._id);
-        total = total + subscription.count * plan.amount;
-    }
+  for (let i = 0; i < subscriptions.length; i++) {
+    const subscription = subscriptions[i];
+    const plan = await SubscriptionPlan.findById(subscription._id);
+    total = total + subscription.count * plan.amount;
+  }
 
-    res.status(200).send({
-        userCount: userCount,
-        buyerCount: buyerCount,
-        sellerCount: sellerCount,
-        itemCount: itemCount,
-        activeSellerCount: activeSellerCount,
-        subscriptionRevenue: total,
-    });
+  res.status(200).send({
+    userCount: userCount,
+    buyerCount: buyerCount,
+    sellerCount: sellerCount,
+    itemCount: itemCount,
+    activeSellerCount: activeSellerCount,
+    subscriptionRevenue: total,
+  });
 });
 
 module.exports = app;
