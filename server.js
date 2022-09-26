0806a5b65464024df6745acc87e0c13b55c163fb
Arshad Danish <arshaddanishthana@gmail.com>
2022-09-26 09:54:14 +0530

    up

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/build"));
 
 // DB connection
 mongoose.connect(process.env.DB_URI);
@@ -20,7 +21,7 @@ conn.once("open", function () {
 
 // Serve react application
 app.get("/", (req, res) => {
-  res.send("React App!");
+  res.sendFile("/index.html");
 });
 
 // APIs
